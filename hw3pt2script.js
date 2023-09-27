a50b7c70fbc59e688c66e5dcbb40b44a8252ecd5
Tessa Anselm <tessa.anselm@gmail.com>
2023-09-27 10:12:57 -0700

    update 9/27 for homework3

--- hw3pt2script.js
+++ hw3pt2script.js
@@ -1,17 +1,18 @@
-<script>
-        // jQuery validation code
-        jQuery.validator.setDefaults({
-            debug: false, 
-            success: "valid",
-            errorClass: "error-text"
-        });
+$(document).ready(function () {
+// jQuery validation code
+// jQuery validation code
+jQuery.validator.setDefaults({
+    debug: true,
+    success: "valid",
+    errorClass: "error-text" // Apply the "error-text" class to error messages
+});
 
-       // Create an array of item field names
-       var itemFields = ["item1", "item2", "item3", "item4"];
-        var itemValidationMessages = {}
+// Create an array of item field names
+var itemFields = ["item1", "item2", "item3", "item4"];
+var itemValidationMessages = {}
 
-       // Create an object to store validation rules for all item fields
-var itemValidationRules = {};
+// Create an object to store validation rules for all item fields
+var itemValidationRules = {}; 
 
 // Validation rules and messages for each item field
 for (var i = 1; i <= 4; i++) {
@@ -20,103 +21,72 @@ for (var i = 1; i <= 4; i++) {
         required: true,
         number: true,
         min: 0
-    };
-    itemValidationMessages[itemFieldName] = {
-        required: "&nbsp;Please enter a quantity for Item " + i + "!",
-        //number: "Numbers only for Item " + i + "!",
-        //min: "Positive number only for Item " + i + "!"
+    },
+        itemValidationMessages[itemFieldName] = {
+        required: "Please enter a quantity for Item " + i + "!",
+        number: "Numbers only for Item " + i + "!",
+        min: "Positive number only for Item " + i + "!"
     };
 }
 
-// Add validation rule and message for sellerName
-itemValidationRules["sellerName"] = {
-    required: true // Name required
-};
-itemValidationMessages["sellerName"] = {
-    required: "&nbsp;&nbsp;Please enter the seller's name!" // Customize message for name
-};
+// Combine itemValidationRules and itemValidationMessages with sellerName validation
+var combinedValidationRules = Object.assign({}, itemValidationRules, {
+    sellerName: {
+        required: true,
+        minlength: 2
+    }
+});
+
+var combinedValidationMessages = Object.assign({}, itemValidationMessages, {
+    sellerName: {
+        required: "Please enter a name!",
+        minlength: "Name must be at least 2 characters long!"
+    }
+});
 
 $("#inputForm").validate({
-    rules: itemValidationRules,
-    messages: itemValidationMessages,
+    rules: combinedValidationRules,
+    messages: combinedValidationMessages,
     submitHandler: function (form) {
+        updateNumSold();
         updateItemTotals();
-        sumCommission();
         return false; // Prevent form submission
     }
 });
 
+// Function to update numSold values
+function updateNumSold() {
+    $('#numSold1').val($('#item1').val());
+    $('#numSold2').val($('#item2').val());
+    $('#numSold3').val($('#item3').val());
+    $('#numSold4').val($('#item4').val());
+}
 
-        $("#inputForm").validate({
-            rules: itemValidationRules,
-            messages: itemValidationMessages,
-            submitHandler: function (form) {
-                updateItemTotals();
-                sumCommission();
-                return false; // Prevent form submission
-            }
-        });
+// Event listener for the "CLEAR FORM" button
+$('#clearForm').click(function () {
+    $('#inputForm')[0].reset();
+    $('.numField, .right-table').val('');
+});
 
-        // Event listener for the "CLEAR FORM" button
-        $('#clearForm').click(function () {
-            // Reset the form
-            $('#inputForm')[0].reset();
-            // Clear the calculated fields
-            $('.numField, .right-table').val('');
-        });
+function updateItemTotals() {
+    let prices = [20.99, 12.75, 9.95, 35.89];
+    for (var i = 1; i <= 4; i++) {
+        var quantity = parseFloat($('#item' + i).val());
+        var total = (quantity * prices[i - 1]).toFixed(2);
+        $('#total' + i).val('$' + total);
+    }
+    sumCommission();
+}
 
-
-        // Listen for changes in item1-4 fields
-        $(document).ready(function () {
-            $( document ).tooltip();
-            for (var i = 1; i <= 4; i++) {
-                $('#item' + i).on('input', function () {
-                    var itemId = $(this).data('item'); // Get the item number from data-item attribute
-                    var quantity = parseFloat($(this).val()); // Parse the input value as a float
-                    if (!isNaN(quantity) && quantity > 0) {
-                        // If the input is a valid positive number, update numSold1-4 with the valid quantity
-                        $('#numSold' + itemId).val(quantity);
-                    } else {
-                        // If the input is not valid (negative or non-numeric), reset it to 0
-                        $(this).val('0');
-                        $('#numSold' + itemId).val('0');
-                    }
-                    updateItemTotals(); // Recalculate item totals
-                });
-            }
-        });
-
-        // Function to update item totals
-        function updateItemTotals() {
-            let prices = [20.99, 12.75, 9.95, 35.89];
-            for (var i = 1; i <= 4; i++) {
-                var itemId = i; // No need to get the data-item attribute again
-                var quantity = parseFloat($('#item' + i).val());
-                if (!isNaN(quantity) && quantity >= 0) {
-                    var total = (quantity * prices[i - 1]).toFixed(2);
-                    $('#total' + i).val('$' + total);
-                } else {
-                    $('#total' + i).val('');
-                }
-            }
-        }
-
-        // Calculate and update the commission
-        function sumCommission() {
-            var totalSales = 0;
-
-            // Loop through total1 to total4 and calculate the total sales
-            for (var i = 1; i <= 4; i++) {
-                var total = parseFloat($('#total' + i).val().replace('$', '')) || 0;
-                totalSales += total;
-            }
-
-            // Calculate the commission (9% of totalSales + 250)
-            var commission = (0.09 * totalSales) + 250;
-
-            // Update the totalSold and commission for seller
-            $('#totalSold').val('$ ' + totalSales.toFixed(2)); // Add the "$" symbol before the totalSales value
-            $('#totalEarn').val('$ ' + commission.toFixed(2)); // Display with 2 decimal places
-        }
-    
-    </script>
+// Calculate and update the commission
+function sumCommission() {
+    var totalSales = 0;
+    for (var i = 1; i <= 4; i++) {
+        var total = parseFloat($('#total' + i).val().replace('$', '')) || 0;
+        totalSales += total;
+    }
+    var commission = (0.09 * totalSales) + 250;
+    $('#totalSold').val('$ ' + totalSales.toFixed(2));
+    $('#totalEarn').val('$ ' + commission.toFixed(2));
+}
+});
