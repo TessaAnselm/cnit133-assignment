61214b0432d52fdc7e903bc7795d2ec03304b1f1
Tessa Anselm <tessa.anselm@gmail.com>
2023-10-25 00:49:20 -0700

    Modified footer style

--- dummy.js
+++ dummy.js
@@ -1,42 +1,39 @@
-// Validate input
 $(document).ready(function(){
-    // Validation using jQuery Validation Plugin
     $("#asteriskSquareForm").validate({
         rules: {
             userInput: {
                 required: true,
                 number: true,
-                range: [2,10]
+                range: [2, 10]
             }         
         },
         messages: {
             userInput: {
                 required: "Please enter a number between 2-10!",
                 number: "Please enter numerical values only!",
-                range: "Please enter values 2-10 only!"
+                range: "Please enter values between 2-10 only!"
             }  
         },
         submitHandler: function(form) {
-            // When form is valid, call CreateBox function to create asterisk box
             const userInput = parseInt($("#userInput").val());
             const boxHtml = createBox(userInput);
             $("#asteriskSquareResult").html(boxHtml);
-
+            return false; // prevent default form submission
         }
-})
-
-function createBox(number) {
-    let result = '';
-    for (let row = 0; row < number; row++) {
-        for (let column = 0; column < number; column++) {
-            if (row === 0 || row === (number - 1) || column === 0 || column === (number - 1)) {
-                result += '* '; // * for first and last column and top and bottom row
-            } else {
-                result += '&nbsp;&nbsp;'; // Empty resulting inside box
+    });
+    
+    function createBox(number) {
+        let result = '';
+        for (let row = 0; row < number; row++) {
+            for (let column = 0; column < number; column++) {
+                if (row === 0 || row === (number - 1) || column === 0 || column === (number - 1)) {
+                    result += '* '; // * for first and last column and top and bottom row
+                } else {
+                    result += '&nbsp;&nbsp;'; // Empty space inside box
+                }
             }
+            result += '<br>'; // Next line
         }
-        result += '<br>'; // Next line
+        return result;
     }
-    return result
-}
 });
--- hw4script.js
+++ hw4script.js
@@ -179,3 +179,46 @@ document.addEventListener("DOMContentLoaded", function () {
         createTable(interestRates[i]);
     }
 });
+
+
+// more elegant way of coding using jQuery
+    /*
+    $(document).ready(function(){
+        $("#asteriskSquareForm").validate({
+            rules: {
+                userInput: {
+                    required: true,
+                    number: true,
+                    range: [2, 10]
+                }         
+            },
+            messages: {
+                userInput: {
+                    required: "Please enter a number between 2-10!",
+                    number: "Please enter numerical values only!",
+                    range: "Please enter values between 2-10 only!"
+                }  
+            },
+            submitHandler: function(form) {
+                const userInput = parseInt($("#userInput").val());
+                const boxHtml = createBox(userInput);
+                $("#asteriskSquareResult").html(boxHtml);
+                return false; // prevent default form submission
+            }
+        });
+        
+        function createBox(number) {
+            let result = '';
+            for (let row = 0; row < number; row++) {
+                for (let column = 0; column < number; column++) {
+                    if (row === 0 || row === (number - 1) || column === 0 || column === (number - 1)) {
+                        result += '* '; // * for first and last column and top and bottom row
+                    } else {
+                        result += '&nbsp;&nbsp;'; // Empty space inside box
+                    }
+                }
+                result += '<br>'; // Next line
+            }
+            return result;
+        }
+    }); */
